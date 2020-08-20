5165502c5ade089fc1628b0ec08c613622ab5b53
Sam Yang <samyang8989@gmail.com>
2020-08-20 01:57:01 +0000

    Sanitize by using parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -52,18 +52,19 @@ const pool = new Pool({
 // const cohortName = process.argv[2];
 // const resultLimit = process.argv[3];
 
-const [, , cohortName, resultLimit] = process.argv;
+//> node students.js FEB 5
+const [, , cohortName, limit] = process.argv;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id, students.name as name, cohorts.name as cohorts
+  FROM students
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+`;
 
 pool
-	.query(
-		`
-SELECT students.id, students.name as name, cohorts.name as cohorts
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${resultLimit};
-`
-	)
+	.query(queryString, values)
 	.then((res) => {
 		res.rows.forEach((user) => {
 			console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohorts} cohort.`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,20 @@ const pool = new Pool({
 	database: 'bootcampx',
 });
 
+// > node teachers.js JUL
 const [, , cohortName] = process.argv;
-
-pool
-	.query(
-		`
+const values = [`%${cohortName}%`];
+const queryString = ` 
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${cohortName}'
-  ORDER BY teacher;
-`
-	)
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher;`;
+
+pool
+	.query(queryString, values)
 	.then((res) => {
 		res.rows.forEach((user) => {
 			console.log(`${user.cohort}: ${user.teacher}`);
